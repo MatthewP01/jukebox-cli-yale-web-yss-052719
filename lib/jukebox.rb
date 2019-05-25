15d7ac62242e88d1316cbf24d335c05ffd8b7950
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-25 03:32:44 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -36,7 +36,7 @@ def play(songs)
   # song_num = songs[i].first
   songs.each_with_index do |item, index|
     if song_choice.to_i == index
-      puts "Playing #{songs[index]}"
+      puts "Playing #{songs[index - 1]}"
     elsif song_choice == item
       puts "Playing #{songs[index]}"
     else
